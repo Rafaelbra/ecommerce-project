1eca9b9d236f4aa6369b4b4231488f907712dc15
Rafael Braga <rafaelbra149@gmail.com>
2025-07-25 16:21:13 -0400

    refactoring-filters

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import { Footer } from './components/Footer'
 import ProductGrid from './components/ProductGrid'
 import { useState } from 'react'
+import Filters from './components/Filters'
 
 const products = [
   {id: 1, title: 'Smart Phone', price:666, url:''},
@@ -11,44 +12,31 @@ const products = [
   {id: 4, title: 'Watch', price:369, url:''},
 ]
 
-
 function App() {
   const [filteredProducts, setFilteredProducts] = useState(products)
 
-  const showFilteredProducts = () => {
-    setFilteredProducts(products.filter(product => product.price < 500))
-  }
-
-  const showAllProducts = () => {
-    setFilteredProducts(products)
-  }
+  const handleFilter = (filterType) => {
+    if (filterType === 'under500') {
+      setFilteredProducts(products.filter(product => product.price < 500))
+    } else {
+      setFilteredProducts(products)
+    }
+  };
 
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
 
       <main className='flex-grow flex bg-gray-100'>
-        <aside className='w-1/4 p-4 bg-white pb-4'>
-        <h2 className='text-lg font-semibold mb-4'>Filters</h2>
-          <button className='block w-full px-4 py-2 my-2 text-white rounded bg-blue-500'
-          onClick={showFilteredProducts}
-          >
-            Under $500</button>
-          <button className='block w-full px-4 py-2 my-2 text-white rounded bg-gray-500'
-          onClick={showAllProducts}
-          >
-            Show all products</button>
-        </aside>
+        <Filters onFilter={handleFilter}/>
 
         <section className='flex-grow p-4'>
           <ProductGrid products={filteredProducts}/>
         </section>
       </main>
-      
 
       <Footer />
     </div>
-    
   )
 }
 
