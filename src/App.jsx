b1bfe26d4c012a052e65f3ffc240889f817cabdd
Rafael Braga <rafaelbra149@gmail.com>
2025-07-25 14:51:27 -0400

    filtering-with-useState

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,14 @@ const products = [
 function App() {
   const [filteredProducts, setFilteredProducts] = useState(products)
 
+  const showFilteredProducts = () => {
+    setFilteredProducts(products.filter(product => product.price < 500))
+  }
+
+  const showAllProducts = () => {
+    setFilteredProducts(products)
+  }
+
   return (
     <div className='flex flex-col min-h-screen'>
       <Header />
@@ -22,8 +30,14 @@ function App() {
       <main className='flex-grow flex bg-gray-100'>
         <aside className='w-1/4 p-4 bg-white pb-4'>
         <h2 className='text-lg font-semibold mb-4'>Filters</h2>
-          <button className='block w-full px-4 py-2 my-2 text-white rounded bg-blue-500'>Under $500</button>
-          <button className='block w-full px-4 py-2 my-2 text-white rounded bg-gray-500'>Show all products</button>
+          <button className='block w-full px-4 py-2 my-2 text-white rounded bg-blue-500'
+          onClick={showFilteredProducts}
+          >
+            Under $500</button>
+          <button className='block w-full px-4 py-2 my-2 text-white rounded bg-gray-500'
+          onClick={showAllProducts}
+          >
+            Show all products</button>
         </aside>
 
         <section className='flex-grow p-4'>
