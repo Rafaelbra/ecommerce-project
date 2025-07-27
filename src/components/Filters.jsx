4bd46d04aa8b5ed3fc77012e6e14618f973e8103
Rafael Braga <rafaelbra149@gmail.com>
2025-07-27 15:08:09 -0400

    Enhancing the Filtering Functionality

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,29 +1,47 @@
 import { useQuery } from '@tanstack/react-query';
 import PropTypes from 'prop-types'
+import axios from 'axios';
 
 const fetchCategories = async () => {
     try {
         const { data } = await axios.get('https://fakestoreapi.com/products/categories');
         return data;
     } catch (error) {
-        console.log(error);
-    }
+        console.log('Error fetching categories:',error);
+    } return []; // Return an empty array on error
 }
+
 
 const Filters = ({onFilter}) => {
     const {data, error, isLoading} = useQuery({
     queryKey:['categories'],
     queryFn: fetchCategories,
 });
-// console.log({data, error, isLoading})
+
+if (isLoading) return <aside className="w-1/4 p-4 bg-white">Loading categories...</aside>;
+if (error) return <aside className="w-1/4 p-4 bg-white">Failed to load categories</aside>;
+
 
     return(
         <aside className='w-1/4 p-4 bg-white pb-4'>
-            <h2 className='text-lg font-semibold    mb-4'>Filters</h2>
-            <button className='block w-full px-4 py-2 my-2 text-white rounded bg-blue-500' onClick={() => onFilter('under500')}>
-            Under $500
-            </button>
-            <button className='block w-full px-4 py-2 my-2 text-white rounded bg-gray-500' onClick={() => onFilter('all')}>Show all products</button>
+
+            <div>
+                <h3 className='text-lg font-semibold mb-4'>Categories</h3>
+                <div className='flex flex-wrap gap-2'>
+                    <button onClick={() => onFilter({ category: ''})} className='px-3 py-1 rounded-full bg-gray-200 hover:bg-gray-300'> 
+                        All products
+                    </button>
+                    {data.map((category) => (
+                        <button 
+                         key={category} 
+                         onClick={() => onFilter({ category })}
+                         className='px-3 py-1 rounded-full bg-gray-200 hover:bg-gray-300 capitalize'
+                        > 
+                        {category}
+                        </button>
+                    ))}
+                </div>
+            </div>
         </aside>
     )
 }
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,23 +2,32 @@ import ProductGrid from './ProductGrid'
 import axios from 'axios'
 import { useQuery } from '@tanstack/react-query';
 import Filters from './Filters';
+import { useState } from 'react';
 
 
 
-const fetchProducts = async () => {
+const fetchProducts = async ({ queryKey }) => {
+    const [key, filters] = queryKey
+    let url = 'https://fakestoreapi.com/products'
+    if (filters.category) {
+        url += `/category/${filters.category}`
+    }
     try {
-        const { data } = await axios.get('https://fakestoreapi.com/products');
+        const { data } = await axios.get(url);
     return data;
     } catch (error) {
         console.log(error);
     }
-}
+};
 
-const productPromise = fetchProducts();
 
 const Products = () => {
+    const [filters, setFilters] = useState({
+        category: ''
+    })
+
     const {data, error, isLoading} = useQuery({
-        queryKey:['products'],
+        queryKey:['products', filters],
         queryFn: fetchProducts,
     });
 
@@ -27,7 +36,7 @@ const Products = () => {
 
   return (
     <main className='flex-grow flex bg-gray-100'>
-        <Filters onFilter={() => {}}/>
+        <Filters onFilter={setFilters}/>
         <ProductGrid products={data}/> 
     </main>
     
