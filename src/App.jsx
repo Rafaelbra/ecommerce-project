a31d8d5b5e1b0c3ca182a433dab452a806873c43
Rafael Braga <rafaelbra149@gmail.com>
2025-07-28 17:59:48 -0400

    Setting up React Router

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,32 @@ import Header from './components/Header'
 import { Footer } from './components/Footer'
 import Products from './components/Products'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { Outlet, Route, Routes } from 'react-router'
 
 const queryClient = new QueryClient();
 
-function App() {
- return (
+function Layout() {
+  return (
     <div className='flex flex-col min-h-screen'>
-
       <Header />    
-
-        <QueryClientProvider client={queryClient}>
-         <Products />
-        </QueryClientProvider>
-
+      <div className='flex-1'>
+        <Outlet />
+      </div>
       <Footer />
     </div>
   );
 }
 
-
+function App() {
+ return (
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path='/' element={<Products />} />
+        </Route>
+      </Routes>
+    </QueryClientProvider>
+  );
+}
 
 export default App
