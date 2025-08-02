06019761e527536c395d7b6db6210bbbdbbb422e
Rafael Braga <rafaelbra149@gmail.com>
2025-08-02 14:36:06 -0400

    Checkout page details

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,10 +1,27 @@
+import { use } from "react"
+import { CartContext } from "../context/CartContext"
+
 export default function Checkout() {
-  return (
+  const {items, cartTotal} = use(CartContext);
+
+  if (items.length === 0) return (
     <div className='p-8 max-w-4xl mx-auto'>
-      <h1 className='text-2xl font-bold mb-6'>Checkout</h1>
+      <h1 className='text-2xl font-bold mb-6'>Your cart is empty</h1>
       <div className='bg-white p-6 rounded shadow'>
-        <p className='text-gray-600'>Checkout form will be implemented here</p>
+        <p className='text-gray-600'>Add some items to your cart before checking out.</p>
       </div>
     </div>
   );
+
+  function handleCheckout () {}
+
+  return (
+    <div className="p-8 max-w-4xl mx-auto">
+        <h1 className='text-2xl font-bold mb-6'>Checkout</h1>
+        <div className="grid md:grid-cols-2 gap-8">
+          <CheckoutForm onSubmit={handleCheckout} />
+          <CartSummary items={items} cartTotal={cartTotal} />
+        </div>
+    </div>
+  )
 }
