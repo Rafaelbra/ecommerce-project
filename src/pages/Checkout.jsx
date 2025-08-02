8057bfbfe8199caa6dd4a38a6aa004dedc367175
Rafael Braga <rafaelbra149@gmail.com>
2025-08-02 17:44:22 -0400

    Finalizing Checkout forms

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -19,7 +19,7 @@ export default function Checkout() {
 
   return (
     <div className="p-8 max-w-4xl mx-auto">
-        <h1 className='text-2xl font-bold mb-6'>Checkout</h1>
+        <h1 className='text-2xl font-bold mb-6 text-'>Checkout</h1>
         <div className="grid md:grid-cols-2 gap-8">
           <CheckoutForm onSubmit={handleCheckout} />
           <CartSummary items={items} cartTotal={cartTotal} />
