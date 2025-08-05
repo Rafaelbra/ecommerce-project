3f91181000d4fd76d63d61559a965896b8863dd3
Rafael Braga <rafaelbra149@gmail.com>
2025-08-05 19:08:07 -0400

    Fix import path case for NotFound

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router';
 import { useProduct } from '../hooks/useProduct';
-import NotFound from './notFound';
+import NotFound from './NotFound';
 import { use, useState } from 'react';
 import { CartContext } from '../context/CartContext';
 import styled from '@emotion/styled';
